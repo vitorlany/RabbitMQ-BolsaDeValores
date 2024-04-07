99ed39ba1c54dd622ddbb6df6f6dc9f4e5d8f492
vitorlany <vitorlany99@gmail.com>
2024-04-07 16:56:57 -0300

    routing key

--- src/main/java/com/project/bolsadevalores/app/messaging/Queue.java
+++ src/main/java/com/project/bolsadevalores/app/messaging/Queue.java
@@ -2,18 +2,24 @@ package com.project.bolsadevalores.app.messaging;
 
 import com.project.bolsadevalores.app.messaging.dto.OrdemMessage;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Queue {
 
     @RabbitListener(queues = "ComprarAcao")
-    public void comprarAcao(final OrdemMessage content) {
+    public void comprarAcao(final OrdemMessage content,
+                            @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String key) {
         System.out.println("Comprar: " + content);
+        System.out.println("Key: " + key);
     }
 
     @RabbitListener(queues = "VenderAcao")
-    public void venderAcao(final OrdemMessage content) {
+    public void venderAcao(final OrdemMessage content,
+                           @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String key) {
         System.out.println("Vender: " + content);
+        System.out.println("Key: " + key);
     }
 }
