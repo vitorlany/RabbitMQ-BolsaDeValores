9e8cdcac093d728c3458fb373d5cf40e766f511c
vitorlany <vitorlany99@gmail.com>
2024-04-09 00:59:23 -0300

    nome métodos

--- src/main/java/com/project/bolsadevalores/app/messaging/Queue.java
+++ src/main/java/com/project/bolsadevalores/app/messaging/Queue.java
@@ -3,29 +3,32 @@ package com.project.bolsadevalores.app.messaging;
 import com.project.bolsadevalores.app.domain.Ordem;
 import com.project.bolsadevalores.app.messaging.dto.OrdemMessage;
 import com.project.bolsadevalores.app.service.BolsaDeValores;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class Queue {
 
     //comprar.petr4
     //{"quantidade": 5, "valor": 135.5, "corretora": "XPTO"}
     @RabbitListener(queues = "ComprarAcao")
-    public void comprarAcao(final OrdemMessage content,
-                            @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
+    public void receberCompraDeAcao(final OrdemMessage content,
+                                    @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
         String codigoDeAcao = routingKey.replaceFirst("comprar.", "");
         Ordem ordem = new Ordem(content.quantidade(), content.valor(), content.corretora());
         BolsaDeValores.comprarAcao(ordem, codigoDeAcao);
     }
 
     @RabbitListener(queues = "VenderAcao")
-    public void venderAcao(final OrdemMessage content,
-                           @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
+    public void receberVendaDeAcao(final OrdemMessage content,
+                                   @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
         String codigoDeAcao = routingKey.replaceFirst("vender.", "");
         Ordem ordem = new Ordem(content.quantidade(), content.valor(), content.corretora());
         BolsaDeValores.venderAcao(ordem, codigoDeAcao);
     }
+    
 }
